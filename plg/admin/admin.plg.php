88c467933c4b82f72dede3bd82bf52303df074f0
Rev. Daniel Carlson <9057136+petrakid@users.noreply.github.com>
2019-08-21 08:41:35 -0500

    continuing in policies added js and ajax

--- plg/admin/admin.plg.php
+++ plg/admin/admin.plg.php
@@ -397,6 +397,7 @@ if(!isset($_SESSION['isLoggedIn'])) {
                     'policy': pol,
                },
                success: function(data) {
+                    alert('#' + pol + 'res');
                     $('#' + pol + 'res').html(data);
                },
           })
