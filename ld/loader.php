2e94886b869bf110417fd498799ff543c1b0a6c2
Rev. Daniel Carlson <9057136+petrakid@users.noreply.github.com>
2019-08-21 08:11:28 -0500

    add policy admin

--- ld/loader.php
+++ ld/loader.php
@@ -1,48 +1,29 @@
 <?php
 session_start();
-
-if(is_file('inst/install.php')) {
-     include 'inst/install.php';
-     die;
-} else {
-     include 'ld/db.inc.php';
-}
-
-if(!empty($_COOKIE['remlog'])) {
+include 'ld/db.inc.php';
+if(!empty($_COOKIE['remlog']) && $_COOKIE['remlog'] == TRUE) {
      $usr = $db->query("SELECT * FROM tbl_users WHERE cookie_hash = '$_COOKIE[remlog]' AND account_status = 1");
-     if($usr->rowCount() < 1) {
-          //echo 'User not found or disabled, or you cleared your browser\'s cache and cleared cookies.';
-     } else {
-          $row = $usr->fetch(PDO::FETCH_ASSOC);               
+     if($usr->rowCount() == 1) {
+          $row = $usr->fetch(PDO::FETCH_ASSOC);
           $_SESSION['isLoggedIn'] = 1;
           $_SESSION['user'] = $row;
           $_SESSION['upload_year'] = date("Y");
      }
 }
-
 include 'ld/globals.inc.php';
 include 'ld/addins.inc.php';
-
-// before we go any further, we want to check and see if maintenance mode is enabled, and if so, load the maintenance page
 if($gbl['maintenance_mode'] == 1 && !isset($_SESSION['isLoggedIn'])) {
      header('location:maintenance.php');
      die;
 }
-
-// load the cron system
 include 'ld/cron_script.php';
-
-// check to see if resources directory exists, and if not, create it
 $year = date("Y");
 if(!file_exists($gbl['doc_root'] ."ast/res/$year")) {
      mkdir($gbl['doc_root'] ."ast/res/$year");
 }
-
-// Load the page data into an array
 if(!isset($_GET['page']) || $_GET['page'] == '') {
      $_GET['page'] = $gbl['homepage'];
 }
-
 if(strpos($_GET['page'], "/") !== false) {
      $_SESSION['fullpage'] = $_GET['page'];
      $var = explode("/", $_GET['page']);
@@ -51,16 +32,16 @@ if(strpos($_GET['page'], "/") !== false) {
 } else {
      $_SESSION['fullpage'] = $_GET['page'];
 }
-
 $sql = $db->query("SELECT * FROM tbl_pages WHERE menu_link = '$_GET[page]'");
 if($sql->rowCount() == 0) {
      $sql = $db->query("SELECT * FROM tbl_admin_pages WHERE menu_link = '$_GET[page]'");
      if($sql->rowCount() == 0) {
           header("location:". $gbl['site_url'] .'/'. $gbl['homepage']);
           die;
-     }     
+     }
 }
 $pg = $sql->fetch(PDO::FETCH_ASSOC);
+$editable = $pg['editable'];
 $_SESSION['page'] = $pg;
 
 include 'lo/head.inc.php';
